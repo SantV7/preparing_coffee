a67e75a6959c9ed526b090cf9d5906ea5acca1ca
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-04-27 00:22:32 -0300

    Criando a lista de itens e as tipagens

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
+import { useState } from "react"
+
+interface listCoffeeProps {
+  id: number,
+  coffe: string,
+  acucar: boolean
+}
 
 function App() {
+
+  const [listCoffe] = useState<listCoffeeProps[]>([
+    {id: 1, coffe: 'Café expresso', acucar: false},
+    {id: 2, coffe: 'Cappucino', acucar: true},
+  ])
   
+ console.log(listCoffe)
 
   return (
     <>
