2ad3940aabe50ccdfa5e3ba0ff01db3745013686
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-04-28 19:46:12 -0300

    Adicionandoc as descrições dos cafés

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,10 @@ interface listCoffeeProps {
 function App() {
 
   const [listCoffe] = useState<listCoffeeProps[]>([
-    {id: 1, coffe: 'Café expresso', description: 'a' ,acucar: false},
-    {id: 2, coffe: 'Cappucino', description: 'a' ,acucar: true},
-    {id: 3, coffe: 'Gray Coffee', description: 'a' ,acucar: false},
-    {id: 4, coffe: 'American Choco', description: 'a' ,acucar: true},
+    {id: 1, coffe: 'Café expresso', description: 'O clássico em sua forma mais pura. Este expresso é para quem aprecia a intensidade do grão torrado e o corpo denso do café. Com notas levemente amendoadas e uma acidez equilibrada, ele entrega o "puro suco" de energia para começar o dia com o pé direito. Sem distrações, apenas café de verdade.' ,acucar: false},
+    {id: 2, coffe: 'Cappucino', description: 'A combinação perfeita entre o vigor do café e a cremosidade do leite vaporizado. Finalizado com uma camada generosa de espuma, esta versão já vem adoçada na medida certa para realçar o sabor do chocolate e da canela que compõem a receita tradicional. É o abraço quente que você merece no meio da tarde.' ,acucar: true},
+    {id: 3, coffe: 'Gray Coffee', description: 'A combinação perfeita entre o vigor do café e a cremosidade do leite vaporizado. Finalizado com uma camada generosa de espuma, esta versão já vem adoçada na medida certa para realçar o sabor do chocolate e da canela que compõem a receita tradicional. É o abraço quente que você merece no meio da tarde.' ,acucar: false},
+    {id: 4, coffe: 'American Choco', description: 'O melhor dos dois mundos. Imagine um café americano (mais suave e longo) fundido com a indulgência de um chocolate quente cremoso. Esta bebida é doce, reconfortante e levemente aveludada. É a escolha ideal para quem quer o sabor do chocolate com o "punch" de cafeína de um café filtrado de alta qualidade.' ,acucar: true},
   ])
 
   const [freebieWon, setFreebieWon] = useState<string>('')
